ba653abd914f3d56600210b8778cc4f4a1c0da44
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2021-09-13 18:14:07 +0800

    customize: docker-compose.yml: tweak envs, add volumes, update tags, and add scripts

---
+++ examples/init.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+VOLUMES_ROOT=/data/docker/volumes/fusiondirectory
+mkdir -p ${VOLUMES_ROOT}/logs
+#mkdir -p ${VOLUMES_ROOT}/custom
+#mkdir -p ${VOLUMES_ROOT}/plugins-custom
+
---
+++ examples/up.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+VOLUMES_ROOT=/data/docker/volumes/fusiondirectory \
+docker compose up -d
